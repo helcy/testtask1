bc43ff32bfd93d86f3debd60ed8eba9d53012617
helcy <oleg@peakfactor.com>
2022-03-15 17:35:04 +0100

    Fixed controller to the last Symfony version method detection

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +13,14 @@ class EventsController extends AbstractController
     #[Route('/events/{eventId}/register', name: 'app_events', methods: ['GET', 'POST'])]
     public function register(int $eventId): Response
     {
-        if ($this->getRequest()->isMethod('GET')) {
+        if (Request::createFromGlobals()->getMethod() === 'GET') {
             // TODO: show registration form in HTML based on $formConfig
             $formConfig = (new Events())->getRegistrationForm($eventId);
-        } else {
-            // TODO: show the invoice in plain text
+
+            return new Response();
         }
+
+        // TODO: show the invoice in plain text
+        return new Response();
     }
 }
